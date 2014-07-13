7a4beff3257d24bc5be9cb137e2f463fa6633944
Cody Scott <austics@gmail.com>
2014-07-12 23:56:26 -0500

    styling of comment header

--- hnidenticon.js
+++ hnidenticon.js
@@ -30,18 +30,20 @@ function buildIdenticon(str) {
     var color = getRGB(str);
     var first = str.charAt(0).toUpperCase();
 
-    return "<div style='background-color:" + color + "; width: 30px; height:30px; float:left; margin: 2px 2px 0px;' >"
+    return "<div style='background-color:" + color + "; width: 30px; height:30px; float:left; margin: -1px 6px 0px 0px;' >"
          + "<p style='font-weight:bold;font-size:12;text-align:center;vertical-align:middle;color:#FFFFFF''>" + first + "</p>" + "</div>"
 }
 
 $("a[href^=user]").each(function () {
     // change margin to fit
     var div = $(this).closest('div');
-    div.css('margin-top', '2px')
-    div.css('margin-bottom', '5px')
+    div.css('margin-top', '0px')
+    div.css('margin-bottom', '-10px')
 
     var str = $(this).text();
     var identicon = buildIdenticon(str);
 
+    $(this).empty()
     $(this).append(identicon)
+    $(this).append("<p>"+str+"</p>")
 })
